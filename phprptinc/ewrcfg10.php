762a037e22cf974995bba545fe5793cad9b3a049
pc_it_storage <ambicopayroll@gmail.com>
2017-05-30 16:05:49 +0700

    1

--- phprptinc/ewrcfg10.php
+++ phprptinc/ewrcfg10.php
@@ -79,7 +79,7 @@ define("EWR_SESSION_TIMEOUT_COUNTDOWN", 60, TRUE); // Session timeout count down
 // General
 $EWR_ROOT_RELATIVE_PATH = "."; // Relative path of app root
 define("EWR_UNFORMAT_YEAR", 50, TRUE); // Unformat year
-define("EWR_RANDOM_KEY", 'w2Htile8Ve3axUg6', TRUE); // Random key for encryption
+define("EWR_RANDOM_KEY", 'ni8NoZNEHB3spsw9', TRUE); // Random key for encryption
 define("EWR_PROJECT_STYLESHEET_FILENAME", "phprptcss/tetrasma.css", TRUE); // Project stylesheet file name
 define("EWR_CHART_WIDTH", 550, TRUE);
 define("EWR_CHART_HEIGHT", 440, TRUE);
